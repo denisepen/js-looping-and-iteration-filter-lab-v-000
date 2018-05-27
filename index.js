cf59f5b6eecc2e905f146ab4bc2909f7d1fb7320
Denise Pendleton <dpendl1@hotmail.com>
2018-05-27 19:23:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const drivers =
 ]
 
 
-function findMatching(drivers, name){
-  newCollection = drivers.filter(function(name)
-   {return  drivers.name == name})
-return newCollection
+// function findMatching(drivers, name){
+//   newCollection = drivers.filter(function(name)
+//    {return  drivers.name == name})
+// return newCollection
+// }
+
+newCollection = drivers.filter(findMatching)
+
+function findMatching(drivers,name){
+  return drivers.name == name
 }
