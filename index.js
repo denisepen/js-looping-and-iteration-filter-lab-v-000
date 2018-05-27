a9c5236f1e5f359469aff5d058253c8078397c97
Denise Pendleton <dpendl1@hotmail.com>
2018-05-27 19:04:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Code your solution in this file
 const drivers =
 [{name: "Bobby"},
-{name: "Bobby"},
+{name: "Fred"},
+{name: "tammy"}
 ]
 
 
 function findMatching(drivers, name){
   newCollection = drivers.filter(function(name)
-   {  name == drivers.name})
+   { name == drivers.name})
 return newCollection
 }
