2557ee489e7ebf86274da14712db1af2fc40585d
Denise Pendleton <dpendl1@hotmail.com>
2018-05-27 18:33:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file
+const drivers = [{name: "betty"}, {name: "dave"}, {name: "frank"}, {name: "sandy"}]
+function findMatching(array, string){
+    array.filter(function(string) return )
+}
